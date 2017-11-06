6181f57af84f282d7f991ba608a1734d969d4175
John Marks <johnjmarks4@gmail.com>
2017-11-05 21:07:24 -0600

    Add edit and update actions to kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,8 +1,16 @@
 class KittensController < ApplicationController
   def new
+    @kitten = Kitten.new
+    if !params[:kitten_id].nil?
+      @kitten_id = params[:kitten_id]
+    end
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to kittens_show_path(@kitten, kitten_id: @kitten.id)
+    end
   end
 
   def show
@@ -20,13 +28,23 @@ class KittensController < ApplicationController
   end
 
   def edit
+    @kitten = Kitten.find(params[:format]) #handle error?
   end
 
   def update
+    @kitten = Kitten.find(params[:kitten_id])
+    @kitten.update(kitten_params)
+    if @kitten.valid?
+      redirect_to kittens_show_path(@kitten, kitten_id: @kitten.id)
+    end
   end
 
   def destroy
     Kitten.find(params[:format]).destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'kittens/create'
 
+  post 'kittens/create' => 'kittens#create'
+
+  post 'kittens' => 'kittens#create'
+
   get 'kittens/show' => 'kittens#show'
 
   post 'kittens/show' => 'kittens#show'
@@ -11,7 +15,9 @@ Rails.application.routes.draw do
 
   get 'kittens/edit'
 
-  get 'kittens/update'
+  post 'kittens/edit'
+
+  patch 'kittens' => 'kittens#update'
 
   root "kittens#index"
 end
